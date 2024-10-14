d547e8042ea89fc0a626f547c0afc6f52afb5128
gbayarri <genis.bayarri@irbbarcelona.org>
2024-10-14 11:01:34 +0200

    Passing .env variables to mongo-init.js

--- mongodb/mongo-init.js
+++ mongodb/mongo-init.js
@@ -1,7 +1,7 @@
 // Switch to the desired database
-db = db.getSiblingDB('DB_NAME'); // Replace 'mddb_db' with your database name
+db = db.getSiblingDB('DB_NAME'); // Replace 'DB_NAME' with your database name
 
-// Create a user with readWrite permissions on 'mddb_db' database. This user will be used for the loader
+// Create a user with readWrite permissions on 'DB_NAME' database. This user will be used for the loader
 db.createUser({
   user: 'LOADER_DB_LOGIN',
   pwd: 'LOADER_DB_PASSWORD',
@@ -10,7 +10,7 @@ db.createUser({
   ]
 });
 
-// Create a user with read permissions on 'mddb_db' database. This user will be used for the REST API
+// Create a user with read permissions on 'DB_NAME' database. This user will be used for the REST API
 db.createUser({
   user: 'REST_DB_LOGIN',
   pwd: 'REST_DB_PASSWORD',
