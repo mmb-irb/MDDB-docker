ab0c629db9923418b7954988dd6284a4a49691ed
gbayarri <genis.bayarri@irbbarcelona.org>
2024-11-26 16:29:37 +0100

    New rebuild script

--- scripts/rebuild.py
+++ scripts/rebuild.py
@@ -1,6 +1,36 @@
-# docker-compose build --no-cache vre_lite --no-cache client
-# docker service update --force my_stack_vre_lite
-# docker service update --force my_stack_client
-# ...
-# docker container prune -f
-# docker image prune -f
+import argparse
+import subprocess
+
+
+def run_command(command):
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Command '{' '.join(command)}' failed with return code {e.returncode}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Rebuild and push one or more services from MDDB node.')
+    parser.add_argument('-s', '--services', nargs='+', help='List of services to build and update')
+
+    args = parser.parse_args()
+
+    # Build services with --no-cache
+    build_command = ['docker-compose', 'build']
+    for service in args.services:
+        build_command.extend(['--no-cache', service])
+    print(f"Running command: {' '.join(build_command)}")
+    run_command(build_command)
+
+    # Update services
+    for service in args.services:
+        update_command = ['docker', 'service', 'update', '--force', service]
+        run_command(update_command)
+
+    # Prune containers and images
+    run_command(['docker', 'container', 'prune', '-f'])
+    run_command(['docker', 'image', 'prune', '-f'])
+
+
+if __name__ == '__main__':
+    main()
