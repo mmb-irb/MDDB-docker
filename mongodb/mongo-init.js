508edbe04dd2d3649b4f925330b77c7bf172f48c
gbayarri <genis.bayarri@irbbarcelona.org>
2024-11-29 12:35:29 +0100

    Fixing mongodb login

--- mongodb/mongo-init.js
+++ mongodb/mongo-init.js
@@ -1,14 +1,22 @@
-// Switch to the desired database
-db = db.getSiblingDB('DB_NAME');
+use admin;
 
-// Create a user with dbOwner permissions on 'DB_NAME' database.
+// TO TEST
+/*const username = process.env.MONGO_INITDB_ROOT_USERNAME;
+const password = process.env.MONGO_INITDB_ROOT_PASSWORD;
+IF IT WORKS; CHECK WITH THE OTHERS!!!
+*/
+
+// Create a user with root permissions on 'admin' database.
 db.createUser({
-  user: 'root',
-  pwd: 'root',
-  roles: [
-    { role: 'dbOwner', db: 'DB_NAME' }]
+  user: 'MONGO_INITDB_ROOT_USERNAME',
+  pwd: 'MONGO_INITDB_ROOT_PASSWORD',
+  roles: [ 
+    { role: 'root', db: 'admin' } 
+  ]
 });
 
+// Switch to the desired database
+db = db.getSiblingDB('DB_NAME');
 
 // Create a user with readWrite permissions on 'DB_NAME' database. This user will be used for the loader
 db.createUser({
