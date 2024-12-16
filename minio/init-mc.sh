81a31e3ba79e2dcd0c2fb9128cc83bcc1f18303d
gbayarri <genis.bayarri@irbbarcelona.org>
2024-12-16 18:11:38 +0100

    MinIO user

--- minio/init-mc.sh
+++ minio/init-mc.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+#!/bin/sh
+
+# Start MinIO server in the background
+minio server --address ":${MINIO_API_INNER_PORT}" --console-address ":${MINIO_UI_INNER_PORT}" http://minio/mnt/disk{1...4}
+
+# Wait for MinIO to initialize
+sleep 5
+
 # Set up MinIO client alias
 mc alias set myminio http://localhost:${MINIO_API_INNER_PORT} ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
 
@@ -8,3 +16,6 @@ mc admin user add myminio ${MINIO_USER} ${MINIO_PASSWORD}
 
 # Set policy for the new user
 mc admin policy attach myminio readwrite --user ${MINIO_USER}
+
+# Wait for MinIO process to finish
+wait
