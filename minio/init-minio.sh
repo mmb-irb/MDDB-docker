22681caa1e873aaaf27bb5355fd352493a228ac1
gbayarri <genis.bayarri@irbbarcelona.org>
2024-12-16 19:28:33 +0100

    MinIO user

--- minio/init-minio.sh
+++ minio/init-minio.sh
@@ -6,16 +6,27 @@ echo "Initializing MinIO client..."
 minio server --address ":${MINIO_API_INNER_PORT}" --console-address ":${MINIO_UI_INNER_PORT}" http://minio/mnt/disk{1...4}
 
 # Wait for MinIO to initialize
-sleep 5
+# sleep 5
+
+# Health check for the MinIO service
+echo "Waiting for MinIO to be healthy..."
+while ! curl -f "http://localhost:${MINIO_API_INNER_PORT}/minio/health/live"; do
+  echo "MinIO is not healthy yet. Waiting..."
+  sleep 5
+done
+echo "MinIO is healthy."
 
 # Set up MinIO client alias
 mc alias set myminio http://localhost:${MINIO_API_INNER_PORT} ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
+echo "MinIO client alias set."
 
 # Create a new user
 mc admin user add myminio ${MINIO_USER} ${MINIO_PASSWORD}
+echo "New user created."
 
 # Set policy for the new user
 mc admin policy attach myminio readwrite --user ${MINIO_USER}
+echo "Policy set for the new user."
 
 # Wait for MinIO process to finish
 wait
