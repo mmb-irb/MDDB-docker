d3a0b03ee218157876f0d505eaa0679c82a7c504
gbayarri <genis.bayarri@irbbarcelona.org>
2025-02-18 16:04:38 +0100

    Minor bug fixes in scripts/rebuild.py

--- scripts/rebuild.py
+++ scripts/rebuild.py
@@ -1,5 +1,6 @@
 import argparse
 import subprocess
+import sys
 
 
 def run_command(command):
@@ -9,15 +10,33 @@ def run_command(command):
         print(f"Command '{' '.join(command)}' failed with return code {e.returncode}")
 
 
+def command_exists(cmd):
+    try:
+        subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+    except FileNotFoundError:
+        return False
+
+
 def main():
-    parser = argparse.ArgumentParser(description='Rebuild and push one or more services from MDDB node.')
+    parser = argparse.ArgumentParser(description='Rebuild and push one or more services from node.')
     parser.add_argument('-s', '--services', nargs='+', required=True, help='List of services to build and push into the stack.')
     parser.add_argument('-t', '--stack', type=str, required=True, help='Name of the stack where the services are running.')
 
     args = parser.parse_args()
 
     # Build services with --no-cache
-    build_command = ['docker-compose', 'build']
+    build_command = []
+    if command_exists(['docker-compose', 'version']):
+        build_command = ['docker-compose', 'build']
+    elif command_exists(['docker', 'compose', 'version']):
+        build_command = ['docker', 'compose', 'build']
+    else:
+        print("Error: Neither 'docker-compose' nor 'docker compose' commands are available.")
+        sys.exit(1)
+
     for service in args.services:
         build_command.extend(['--no-cache', service])
     print(f"Running command: {' '.join(build_command)}")
